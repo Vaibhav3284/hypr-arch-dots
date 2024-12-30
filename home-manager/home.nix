d011f9be489d1374854caf61552796e3ac992c94
bOred <125344705+Vaibhav3284@users.noreply.github.com>
2024-12-30 18:10:20 +0530

    Add files via upload

---
+++ home-manager/home.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "bored";
+  home.homeDirectory = "/home/bored";
+
+  home.stateVersion = "24.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+     protonup
+  ];
+  
+  home.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\\\${HOME}/.steam/root/compatibilitytools.d";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Vaibhav3284";
+    userEmail = "vaibhavjatoliya1@gmail.com";
+    aliases = {
+      pu = "push";
+      co = "chechout";
+      cm = "commit";
+   };
+  };
+  
+  gtk.enable = true;
+  
+  gtk.cursorTheme.package = pkgs.bibata-cursors;
+  gtk.cursorTheme.name = "Bibata-Modern-Ice";
+
+  gtk.theme.package = pkgs.adw-gtk3;
+  gtk.theme.name = "adw-gtk3";
+
+
+
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
